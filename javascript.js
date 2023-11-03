74bb62ed7362ef3ce3fcf2f9865be2cad0b3d45d
Maria Delfina <mfurio@itba.edu.ar>
2023-11-02 21:17:03 -0300

    commit

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// let altura= (prompt (" ingrese su altura")) 
+
+// let peso= (prompt ("ingrese peso "))
+
+// let bmi=(peso/altura) 
+
+// if (altura<0){ let altura= prompt ('ingrese altura');} 
+
+// elseif (typeof(altura))
+
+// if (peso<0){ let altura =prompt ('ingrese altura');} 
+
+// if bmi <18.5 { console.log (" estas a bajo del peso");} 
+
+// if bmi >18.5 and bmi 
+
+
+// let nombre=prompt ("ingedr")
+
+// document.getElementByaId("tituloprinicipal").innerHTML=
+// "hola nombra";
+
